70b33354773f78675f0ee905d8865d17b0457108
risvil <sabr.risvil@gmail.com>
2026-06-19 17:24:24 +0500

    update policies and schema

--- database/policies.sql
+++ database/policies.sql
@@ -13,3 +13,14 @@ FOR UPDATE USING (auth.uid() = user_id);
 
 CREATE POLICY "Users can delete their own grades" ON grades 
 FOR DELETE USING (auth.uid() = user_id);
+
+ALTER TABLE {your table} ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Users can view own profile" ON public.profiles
+  FOR SELECT USING (auth.uid() = id);
+
+CREATE POLICY "Users can update own profile" ON public.profiles
+  FOR UPDATE USING (auth.uid() = id);
+
+CREATE POLICY "Users can insert own profile" ON public.profiles
+  FOR INSERT WITH CHECK (auth.uid() = id);
--- database/schema.sql
+++ database/schema.sql
@@ -1,21 +1,49 @@
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
-CREATE TABLE public.grades (
-  id uuid NOT NULL DEFAULT gen_random_uuid(),
-  title text NOT NULL,
-  rk1 numeric CHECK (rk1 >= 0::numeric AND rk1 <= 100::numeric),
-  rk2 numeric CHECK (rk2 >= 0::numeric AND rk2 <= 100::numeric),
-  exam numeric CHECK (exam >= 0::numeric AND exam <= 100::numeric),
-  total_percent numeric,
-  created_at timestamp with time zone DEFAULT timezone('utc'::text, now()),
-  fa_grades jsonb,
-  is_pinned boolean,
-  user_id uuid DEFAULT auth.uid(),
-  rk1_note TEXT,
-  rk2_note TEXT,
-  fa_note TEXT,
-  quarter_note TEXT,
-  CONSTRAINT grades_pkey PRIMARY KEY (id),
-  CONSTRAINT grades_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id)
-);
+create table public.grades (
+  id uuid not null default gen_random_uuid (),
+  title text not null,
+  rk1 numeric null,
+  rk2 numeric null,
+  exam numeric null,
+  total_percent numeric null,
+  created_at timestamp with time zone null default timezone ('utc'::text, now()),
+  fa_grades jsonb null,
+  is_pinned boolean null,
+  user_id uuid null default auth.uid (),
+  rk1_note text null,
+  rk2_note text null,
+  fa_note text null,
+  quarter_note text null,
+  course integer not null default 1,
+  semester integer not null default 1,
+  constraint grades_pkey primary key (id),
+  constraint grades_user_id_fkey foreign KEY (user_id) references auth.users (id),
+  constraint check_exam_range check (
+    (
+      (exam >= (0)::numeric)
+      and (exam <= (100)::numeric)
+    )
+  ),
+  constraint check_rk1_range check (
+    (
+      (rk1 >= (0)::numeric)
+      and (rk1 <= (100)::numeric)
+    )
+  ),
+  constraint check_rk2_range check (
+    (
+      (rk2 >= (0)::numeric)
+      and (rk2 <= (100)::numeric)
+    )
+  )
+) TABLESPACE pg_default;
+
+create table public.profiles (
+  id uuid not null,
+  current_course integer null default 1,
+  current_semester integer null default 1,
+  constraint profiles_pkey primary key (id),
+  constraint profiles_id_fkey foreign KEY (id) references auth.users (id) on delete CASCADE
+) TABLESPACE pg_default;
